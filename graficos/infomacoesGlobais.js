25b27bedef7873333b27615f65898a0613281d82
Dereked123 <souza.derek@escola.pr.gov.br>
2025-09-02 08:13:59 -0300

    Update infomacoesGlobais.js

--- graficos/infomacoesGlobais.js
+++ graficos/infomacoesGlobais.js
@@ -1,25 +1,27 @@
-const url="https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json"
+const url = "https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json";
 
 async function visualizarInformacoesGlobais() {
+    const resposta = await fetch(url);
+    const dados = await resposta.json();
 
-    const resposta =await fetch(url)
+    const pessoasMundo = (dados.total_pessoas_mundo / 1e9);
+    const trabalhadoresMundo = (dados.total_trabalhadores_mundo / 1e9);
+    const tempoDesTrabalho = parseInt(dados.tempo_medio_deslocamento_para_trabalho);
+    const minutos = Math.round((dados.tempo_medio_deslocamento_para_trabalho - tempoDesTrabalho) * 60);
 
-    const dados = await resposta.json()
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container_texto');
 
-    const pessoasMundo = (dados.total_pessoas_mundo/1e9)
-    const trabalhadoresMundo = (dados.total_trabalhadores_mundo/1e9)
-    const tempoDesTrabalho = parseInt (dados.tempo_medio_deslocamento_para_trabalho)
-    const minutos = Math.round ((dados.tempo_medio_deslocamento_para_trabalho - tempoDesTrabalho) *60)
+    paragrafo.innerHTML = `
+        O mundo tem <span>${pessoasMundo.toFixed(2)}</span> bilhões de pessoas. Dessas, aproximadamente 
+        <span>${trabalhadoresMundo.toFixed(2)}</span> bilhões estão empregadas e passam, em média, 
+        <span>${tempoDesTrabalho} horas</span> e <span>${minutos} minutos</span> por dia no caminho para o trabalho. 
+        Isso indica que mais da metade da população mundial não exerce trabalhos com vínculos empregatícios legais. 
+        Esse número pode refletir a presença de aposentados, mas também uma alta taxa de trabalho informal.
+    `;
 
-
-
-
-    const paragrafo= document.createElement('p')
-    paragrafo.classList.add('graficos-container_texto')
-
-    paragrafo.innerHTML= `O mundo tem <span>${pessoasMundo}</span>  bilhões de pessoas, dessas pessoas, aproximadamente <span>${trabalhadoresMundo}</span>   bilhões estão empregadas e passam em média  <span>${tempoDesTrabalho}horas</span> e <span>${minutos} minutos</span> por dia no caminho para o trabalho. Temos, portanto, mais da metade da população mundial que não exerce trabalhos com vínculos empregatícios legais. O que pode significar que há longevidade e desse modo, muitas pessoas aposentadas. Porém, também pode significar muitos postos de trabalho chamados informais.`
-    const container=document.getElementById('graficos-container')
+    const container = document.getElementById('graficos-container');
     container.appendChild(paragrafo);
 }
 
-visualizarInformacoesGlobais()
+visualizarInformacoesGlobais();
